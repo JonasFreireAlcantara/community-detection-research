db9e1b0cd9d17fc37ca7968aac44f7f0c39f6481
Jonas Freire <jonasfreireperson@gmail.com>
2021-09-18 11:01:14 -0300

    change of experiments to support multiple metrics

--- experiment_lab/classical_algorithms/experiments/lpa/by_minimum_factor_topologies.py
+++ experiment_lab/classical_algorithms/experiments/lpa/by_minimum_factor_topologies.py
@@ -3,13 +3,19 @@ import os
 from classical_algorithms.algorithms.label_propagation import LabelPropagationAlgorithm
 from classical_algorithms.common.algorithm_applicator import PlatformClassicalRunner
 from common.constants.resources_constants import ResourcesConstants
-from metrics.algorithms.modularity import ModularityMetric
+from metrics.algorithms import ModularityMetric, CoverageMetric, PerformanceMetric, ConductanceMetric
 
 
 if __name__ == '__main__':
     source = os.path.join(ResourcesConstants.DATASET_PATH, 'by_minimum_factor_topologies')
     target = os.path.join('/home/jonas/Desktop/by_minimum_factor_topologies')
+    metrics = [
+        ModularityMetric,
+        CoverageMetric,
+        PerformanceMetric,
+        ConductanceMetric
+    ]
 
-    classical_runner = PlatformClassicalRunner(source, target, LabelPropagationAlgorithm, ModularityMetric)
+    classical_runner = PlatformClassicalRunner(source, target, LabelPropagationAlgorithm, metrics)
     classical_runner.run()
     classical_runner.save_metric()
