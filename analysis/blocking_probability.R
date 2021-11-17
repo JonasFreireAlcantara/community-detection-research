13911fb7bd62518329bf9db66a0a967ec5597430
Jonas Freire <jonasfreireperson@gmail.com>
2021-11-16 21:00:12 -0300

    first generation of dispersion charts

--- analysis/blocking_probability.R
+++ analysis/blocking_probability.R
@@ -1,186 +1,276 @@
+setwd('/home/jonas/Desktop/round_3/')
+
 ######### LPA
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_lpa/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_lpa/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/lpa_modularidade_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$ModularityMetric,
-    main='LPA - EON Blocking Probability vs Modularity'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Coverage
+png(file='images/lpa_coverage_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$CoverageMetric,
-    main='LPA - EON Blocking Probability vs Coverage'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Cobertura',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Performance
+png(file='images/lpa_performance_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$PerformanceMetric,
-    main='LPA - EON Blocking Probability vs Performance'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Performance',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 
 
 ############# Greedy Modularity Optimization
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_greedy_modularity/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_greedy_modularity/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/gmo_modularidade_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$ModularityMetric,
-    main='Greedy Modularity - EON Blocking Probability vs Modularity'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
+plot(
+    x=df$eonBlockingProbability,
+    y=df$ModularityMetric,
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
+)
+lines(c(0.03,0.58), c(0.1,0.45), type='l', lwd=4, col='firebrick1')
+lines(c(0.015,0.5), c(0.13,0.59), type='l', lwd=4, col='firebrick1')
 # Coverage
+png(file='images/gmo_coverage_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$CoverageMetric,
-    main='Greedy Modularity - EON Blocking Probability vs Coverage'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Cobertura',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Performance
+png(file='images/gmo_performance_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$PerformanceMetric,
-    main='Greedy Modularity - EON Blocking Probability vs Performance'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Performance',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 
 
 
 ############# Fluid Communities k=2
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_2/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_2/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/fc2_modularidade_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$ModularityMetric,
-    main='Fluid Communities k=2 - EON Blocking Probability vs Modularity'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Coverage
+png(file='images/fc2_coverage_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$CoverageMetric,
-    main='Fluid Communities k=2 - EON Blocking Probability vs Coverage'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Cobertura',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Performance
+png(file='images/fc2_performance_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$PerformanceMetric,
-    ylim=c(0.4,0.9),
-    main='Fluid Communities k=2 - EON Blocking Probability vs Performance'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Performance',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 
 
 
 
 ############# Fluid Communities k=3
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_3/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_3/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/fc3_modularidade_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$ModularityMetric,
-    main='Fluid Communities k=3 - EON Blocking Probability vs Modularity'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Coverage
+png(file='images/fc3_coverage_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$CoverageMetric,
-    main='Fluid Communities k=3 - EON Blocking Probability vs Coverage'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Cobertura',
+    main='Probabilidade de Bloqueio'
 )
-
+dev.off()
 # Performance
+png(file='images/fc3_performance_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=3 - EON Blocking Probability vs Performance'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Performance',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 
 
 
 
 ############# Fluid Communities k=4
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_4/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_4/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/fc4_modularidade_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$ModularityMetric,
-    ylim=c(0.0, 0.7),
-    main='Fluid Communities k=4 - EON Blocking Probability vs Modularity'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Coverage
+png(file='images/fc4_coverage_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$CoverageMetric,
-    main='Fluid Communities k=4 - EON Blocking Probability vs Coverage'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Cobertura',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Performance
+png(file='images/fc4_performance_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=4 - EON Blocking Probability vs Performance'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Performance',
+    main='Probabilidade de Bloqueio'
 )
-
+dev.off()
 
 
 
 
 ############# Fluid Communities k=5
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_5/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_5/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/fc5_modularidade_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$ModularityMetric,
-    main='Fluid Communities k=5 - EON Blocking Probability vs Modularity'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Coverage
+png(file='images/fc5_coverage_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$CoverageMetric,
-    main='Fluid Communities k=5 - EON Blocking Probability vs Coverage'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Cobertura',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Performance
+png(file='images/fc5_performance_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=5 - EON Blocking Probability vs Performance'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Performance',
+    main='Probabilidade de Bloqueio'
 )
-
+dev.off()
 
 
 
 ############# Fluid Communities k=6
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_6/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_6/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/fc6_modularidade_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$ModularityMetric,
-    main='Fluid Communities k=6 - EON Blocking Probability vs Modularity'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Modularidade',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Coverage
+png(file='images/fc6_coverage_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$CoverageMetric,
-    main='Fluid Communities k=6 - EON Blocking Probability vs Coverage'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Cobertura',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
 # Performance
+png(file='images/fc6_performance_bp.png', width=990, height=819)
 plot(
     x=df$eonBlockingProbability,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=6 - EON Blocking Probability vs Performance'
+    xlab='Probabilidade de Bloqueio',
+    ylab='Performance',
+    main='Probabilidade de Bloqueio'
 )
+dev.off()
--- analysis/resilience.R
+++ analysis/resilience.R
@@ -1,291 +1,472 @@
+setwd('/home/jonas/Desktop/round_2/')
+
 ######### LPA
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_lpa/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_lpa/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+layout.matrix = matrix(c(1), nrow=1, byrow=TRUE)
+layout(mat=layout.matrix)
+png(file='images/lpa_modularidade_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$ModularityMetric,
-    main='LPA - Resilience one link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/lpa_modularidade_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$ModularityMetric,
-    main='LPA - Resilience two link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Dupla'
 )
+dev.off()
+
+
 # Coverage
+png(file='images/lpa_cobertura_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$CoverageMetric,
-    main='LPA - Resilience one link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/lpa_cobertura_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$CoverageMetric,
-    main='LPA - Resilience two link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Performance
+png(file='images/lpa_performance_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$PerformanceMetric,
-    main='LPA - Resilience one link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/lpa_performance_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$PerformanceMetric,
-    main='LPA - Resilience two link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Dupla'
 )
+dev.off()
 
 
 ############# Greedy Modularity Optimization
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_greedy_modularity/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_greedy_modularity/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
-# Modularity
+png(file='images/gmo_modularidade_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$ModularityMetric,
-    main='Greedy Modularity - Resilience one link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/gmo_modularidade_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$ModularityMetric,
-    main='Greedy Modularity - Resilience two link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Coverage
+png(file='images/gmo_cobertura_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$CoverageMetric,
-    main='Greedy Modularity - Resilience one link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/gmo_cobertura_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$CoverageMetric,
-    main='Greedy Modularity - Resilience two link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Performance
+png(file='images/gmo_performance_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$PerformanceMetric,
-    main='Greedy Modularity - Resilience one link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/gmo_performance_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$PerformanceMetric,
-    main='Greedy Modularity - Resilience two link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Dupla'
 )
+dev.off()
 
 
 
 
 ############# Fluid Communities k=2
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_2/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_2/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
-# Modularity
+png(file='images/fc2_modularidade_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=2 - Resilience one link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc2_modularidade_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=2 - Resilience two link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Coverage
+png(file='images/fc2_coverage_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=2 - Resilience one link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc2_coverage_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=2 - Resilience two link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Performance
+png(file='images/fc2_performance_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=2 - Resilience one link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc2_performance_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=2 - Resilience two link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Dupla'
 )
+dev.off()
 
 
 
 
 ############# Fluid Communities k=3
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_3/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_3/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
-# Modularity
+png(file='images/fc3_modularidade_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=3 - Resilience one link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc3_modularidade_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=3 - Resilience two link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Coverage
+png(file='images/fc3_coverage_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=3 - Resilience one link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc3_coverage_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=3 - Resilience two link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Performance
+png(file='images/fc3_performance_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=3 - Resilience one link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc3_performance_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=3 - Resilience two link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Dupla'
 )
+dev.off()
 
 
 
 
 ############# Fluid Communities k=4
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_4/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_4/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
-# Modularity
+png(file='images/fc4_modularidade_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=4 - Resilience one link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc4_modularidade_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=4 - Resilience two link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Coverage
+png(file='images/fc4_coverage_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=4 - Resilience one link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc4_coverage_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=4 - Resilience two link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Performance
+png(file='images/fc4_performance_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=4 - Resilience one link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc4_performance_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$PerformanceMetric,
-    ylim=c(0.5, 0.9),
-    main='Fluid Communities k=4 - Resilience two link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Dupla'
 )
+dev.off()
 
 
 
 
 
 ############# Fluid Communities k=5
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_5/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_5/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
-# Modularity
+png(file='images/fc5_modularidade_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=5 - Resilience one link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc5_modularidade_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=5 - Resilience two link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Coverage
+png(file='images/fc5_coverage_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=5 - Resilience one link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc5_coverage_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=5 - Resilience two link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Performance
+png(file='images/fc5_performance_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=5 - Resilience one link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc5_performance_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=5 - Resilience two link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Dupla'
 )
+dev.off()
 
 
 
 
 ############# Fluid Communities k=6
-metrics_file <- '/home/jonas/Desktop/jonas_si_gml_fluid_communities_k_6/metrics_results.csv'
+metrics_file <- 'jonas_si_gml_fluid_communities_k_6/metrics_results.csv'
 
 df <- read.csv(metrics_file)
 
 # Modularity
+png(file='images/fc6_modularidade_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=6 - Resilience one link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc6_modularidade_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$ModularityMetric,
-    main='Fluid Communities k=6 - Resilience two link vs Modularity'
+    xlab='Resiliência',
+    ylab='Modularidade',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Coverage
+png(file='images/fc6_coverage_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=6 - Resilience one link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc6_coverage_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$CoverageMetric,
-    main='Fluid Communities k=6 - Resilience two link vs Coverage'
+    xlab='Resiliência',
+    ylab='Cobertura',
+    main='Falha Dupla'
 )
+dev.off()
+
 # Performance
+png(file='images/fc6_performance_simples.png', width=990, height=819)
 plot(
     x=df$resilienceOneLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=6 - Resilience one link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Simples'
 )
+dev.off()
+png(file='images/fc6_performance_dupla.png', width=990, height=819)
 plot(
     x=df$resilienceTwoLink,
     y=df$PerformanceMetric,
-    main='Fluid Communities k=6 - Resilience two link vs Performance'
+    xlab='Resiliência',
+    ylab='Performance',
+    main='Falha Dupla'
 )
+dev.off()
 
